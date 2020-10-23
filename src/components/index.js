fc6344d12ee253caadce93d8b5e4480d4624ea87
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-10-23 01:14:33 -0400

    redux-react

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Navbar } from './navbar';
-export { default as Footer } from './user-home';
+export { default as Navbar } from './NavBar';
+export { default as Footer } from './Footer';
 export { default as Home } from './Home';
