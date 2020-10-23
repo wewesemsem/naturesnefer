9b648ad9b9d2834d7bbb67e768295aba46008b90
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-10-23 00:50:45 -0400

    routes

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+function Footer() {
+  return (
+    <Navbar bg="light" variant="light" fixed="bottom" className="footer">
+      <Navbar.Text>© Copyright 2020 - Nature's Nefer</Navbar.Text>
+    </Navbar>
+  );
+}
+
+export default Footer;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return <div>Welcome to Nature's Nefer</div>;
+  }
+}
+
+export default Home;
