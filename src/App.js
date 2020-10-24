2a2eaa9f3c70149cf335769efd5cfcdb767e968f
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-10-23 20:45:17 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import Routes from './routes';
 import Footer from './components/Footer';
 import './App.css';
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Header } from './Header';
+export { default as Header } from './Header/Header';
 export { default as Footer } from './Footer';
 export { default as Home } from './Home';
