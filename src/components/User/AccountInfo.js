cde3176315ed1d88e95b03f9ccf577417692f9cb
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-11-12 13:24:27 -0500

    accordian

--- src/components/User/AccountInfo.js
+++ src/components/User/AccountInfo.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Container, Jumbotron } from 'react-bootstrap';
+import { Container, Accordion, Card, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { logout } from '../../store';
 
 class AccountInfo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pages: [
+        { name: 'Order History', url: '/order-history', eventKey: 1 },
+        { name: 'My Details', url: '/my-details', eventKey: 2 },
+        { name: 'Address Book', url: '/address-book', eventKey: 3 },
+        { name: 'Payment Methods', url: '/payment-methods', eventKey: 4 },
+      ],
+    };
+  }
   render() {
     const handleClick = this.props.handleClick;
     const user = this.props.user;
@@ -14,6 +25,26 @@ class AccountInfo extends React.Component {
       <Container className="Center-column">
         <h1 className="Page-header">Account</h1>
         <div className="Words"> Welcome to your account, {firstName}!</div>
+        <Accordion className="Accordian">
+          {this.state.pages.map((page) => {
+            return (
+              <Card key={page.eventKey}>
+                <Card.Header>
+                  <Accordion.Toggle
+                    as={Button}
+                    variant="link"
+                    eventKey={page.eventKey}
+                  >
+                    {page.name}
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey={page.eventKey}>
+                  <Card.Body>Hello! I'm the body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            );
+          })}
+        </Accordion>
         <a href="/login" className="Logout" onClick={handleClick}>
           Sign Out
         </a>
