90f4defdf80237e6c91e1a0a0052ddb23d00059a
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-11-12 11:40:48 -0500

    account initial frame

--- src/components/User/AccountInfo.js
+++ src/components/User/AccountInfo.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
 class AccountInfo extends React.Component {
   render() {
-    return <div>ACCOUNT</div>;
+    const user = this.props;
+    const firstName = user.user.firstName;
+    return (
+      <Container className="Center-column">
+        <h1 className="Page-header">Account</h1>
+        <div className="Words"> Welcome to your account, {firstName}!</div>
+      </Container>
+    );
   }
 }
 
-export default AccountInfo;
+const mapState = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapState)(AccountInfo);
