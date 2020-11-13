1a3580cfb0c0c3a54dd2c610ecf3b0744d158d70
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-11-13 15:42:24 -0500

    video wrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,18 @@ import React from 'react';
 import movieWebM from '../assets/test.webm';
 import movieMP4 from '../assets/test.mp4';
 import FeaturedProducts from './Products/Featured';
+import { Container } from 'react-bootstrap';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <video preload="auto" autoPlay loop muted className="Video">
-          <source src={movieMP4} type="video/mp4" />
-          <source src={movieWebM} type="video/webm" />
-        </video>
+        <div className="Vid-wrap">
+          <video preload="auto" autoPlay loop muted className="Video">
+            <source src={movieMP4} type="video/mp4" />
+            <source src={movieWebM} type="video/webm" />
+          </video>
+        </div>
         <FeaturedProducts />
       </div>
     );
