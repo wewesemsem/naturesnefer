0672266aa8b67c85142dfbc6c33830b6d92ae114
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-11-13 13:18:07 -0500

    Home frame

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import movieWebM from '../assets/test.webm';
+import movieMP4 from '../assets/test.mp4';
+import FeaturedProducts from './Products/Featured';
 
 class Home extends React.Component {
   render() {
-    return <div>Welcome to Nature's Nefer</div>;
+    return (
+      <div className="Home">
+        <video preload="auto" autoPlay loop muted className="Video">
+          <source src={movieMP4} type="video/mp4" />
+          <source src={movieWebM} type="video/webm" />
+        </video>
+        <FeaturedProducts />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Products/Featured.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+class FeaturedProducts extends React.Component {
+  render() {
+    return (
+      <Container className="Center-column Top-pad">
+        <h1 className="Page-header">Featured Products</h1>
+      </Container>
+    );
+  }
+}
+
+export default FeaturedProducts;
