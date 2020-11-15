72a5ebc324d0e2af955dd1787577629fb32e1748
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2020-11-15 12:39:05 -0500

    sign up confirm email

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,33 @@
 const router = require('express').Router();
 const User = require('../db/models/user');
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  'https://developers.google.com/oauthplayground'
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+});
+
+const accessToken = oauth2Client.getAccessToken();
+
+const smtpTransport = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL_USER,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    accessToken: accessToken,
+  },
+});
 
 router.post('/login', async (req, res, next) => {
   try {
@@ -21,6 +49,13 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body);
+    const message = 'Thanks for signing up,' + user.firstName + '!';
+    const mailOptions = {
+      to: user.email,
+      subject: "Welcome to Nature's Nefer",
+      text: message,
+    };
+    smtpTransport.sendMail(mailOptions);
     req.login(user, (err) => (err ? next(err) : res.json(user)));
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
